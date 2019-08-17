c80b60b3ab2df58ffb073d8a29691156dcc7a220
slavov <kostadin.slavov@tixi.bg>
2019-08-17 11:20:05 +0300

    Add the login/register activity

--- app/src/main/java/com/example/payscan/login_activity.kt
+++ app/src/main/java/com/example/payscan/login_activity.kt
@@ -3,7 +3,7 @@ package com.example.payscan
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class login_activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
